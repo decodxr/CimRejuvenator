1adae429147050d7b5452b10aebc9e8229dd6509
decodxr <155329552+decodxr@users.noreply.github.com>
2026-08-31 13:09:22 -0300

    Remove retired rejuvenation source file

--- RejuvenationSystem.cs
+++
@@ -1,8 +0,0 @@
-namespace CimRejuvenator
-{
-    // Rejuvenation logic moved to PopulationManagementSystem in v0.3.0.
-    // This marker keeps source checkouts from older branches easy to compare.
-    internal static class LegacyRejuvenationSystemMarker
-    {
-    }
-}
