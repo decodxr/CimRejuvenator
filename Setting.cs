93c17dab293b37f99bc35218593875838bb3b1c9
decodxr <155329552+decodxr@users.noreply.github.com>
2026-08-31 00:26:37 -0300

    Use unique settings class name to avoid cross-mod collisions

--- Setting.cs
+++ Setting.cs
@@ -8,7 +8,7 @@ namespace CimRejuvenator
     [FileLocation("ModsSettings/CimRejuvenator/CimRejuvenator")]
     [SettingsUIGroupOrder(kMainGroup, kRejuvenationGroup, kSafetyGroup, kPerformanceGroup, kStatsGroup)]
     [SettingsUIShowGroupName(kMainGroup, kRejuvenationGroup, kSafetyGroup, kPerformanceGroup, kStatsGroup)]
-    public sealed class Setting : ModSetting
+    public sealed class CimRejuvenatorSetting : ModSetting
     {
         public const string kSection = "Main";
         public const string kMainGroup = "General";
@@ -17,7 +17,7 @@ namespace CimRejuvenator
         public const string kPerformanceGroup = "Performance";
         public const string kStatsGroup = "Statistics";
 
-        public Setting(IMod mod) : base(mod)
+        public CimRejuvenatorSetting(IMod mod) : base(mod)
         {
             SetDefaults();
         }
@@ -25,48 +25,48 @@ namespace CimRejuvenator
         [SettingsUISection(kSection, kMainGroup)]
         public bool EnableMod { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISlider(min = 0, max = 100, step = 5, scalarMultiplier = 1, unit = Unit.kPercentage)]
         [SettingsUISection(kSection, kRejuvenationGroup)]
         public int RejuvenationChance { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISlider(min = 36, max = 70, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
         [SettingsUISection(kSection, kRejuvenationGroup)]
         public int ResetAgeDays { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISection(kSection, kRejuvenationGroup)]
         public bool RestoreHealth { get; set; }
 
         [SettingsUIButton]
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISection(kSection, kRejuvenationGroup)]
         public bool RejuvenateNow
         {
             set { RejuvenationSystem.RequestImmediateSweep(); }
         }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISlider(min = 100, max = 250000, step = 1000, scalarMultiplier = 1, unit = Unit.kInteger)]
         [SettingsUISection(kSection, kSafetyGroup)]
         public int MaxRejuvenationsPerDay { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISlider(min = 100, max = 100000, step = 500, scalarMultiplier = 1, unit = Unit.kInteger)]
         [SettingsUISection(kSection, kSafetyGroup)]
         public int MaxRejuvenationsPerSweep { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISection(kSection, kSafetyGroup)]
         public bool KeepMinimumSeniorShare { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsSeniorShareDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsSeniorShareDisabled))]
         [SettingsUISlider(min = 0, max = 50, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
         [SettingsUISection(kSection, kSafetyGroup)]
         public int MinimumSeniorPercent { get; set; }
 
-        [SettingsUIDisableByCondition(typeof(Setting), nameof(IsDisabled))]
+        [SettingsUIDisableByCondition(typeof(CimRejuvenatorSetting), nameof(IsDisabled))]
         [SettingsUISlider(min = 8, max = 256, step = 8, scalarMultiplier = 1, unit = Unit.kInteger)]
         [SettingsUISection(kSection, kPerformanceGroup)]
         public int SweepsPerDay { get; set; }
